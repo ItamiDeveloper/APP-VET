dcd31a8cf3680e05e14945833a2df03b0f0dce91
ItamiDeveloper <carlosedu062010@hotmail.com>
2025-12-01 22:33:45 -0500

    Refactor AppApplication class to implement Spring Boot application structure

--- app/src/main/java/com/vet/spring/app/AppApplication.java
+++ app/src/main/java/com/vet/spring/app/AppApplication.java
@@ -1,5 +1,12 @@
 package com.vet.spring.app;
 
-public enum AppApplication {
-    
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
 }
